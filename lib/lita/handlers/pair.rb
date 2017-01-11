353315392574875d1e7dc92ea75bd7bd4a4eb77d
Ryan Latta <rlatta@thoughtworks.com>
2017-01-11 09:30:07 -0600

    Adding some functionality to create a single pair

--- lib/lita/handlers/pair.rb
+++ lib/lita/handlers/pair.rb
@@ -1,6 +1,7 @@
 module Lita
   module Handlers
     class Pair < Handler
+      REDIS_KEY = 'pair_members'
 
       ###
       # Routes
@@ -13,20 +14,36 @@ module Lita
         remove_user(parse_name(response))
       end
 
-
-      def parse_name(response)
-        response.args[1..-1].join(' ')
+      route(/^pair\s+one/) do |response|
+        pair = create_pair
+        if pair.nil?
+          response.reply('Sorry, I can\'t make a pair out of one person. Try adding more people with pair add')
+        else
+          response.reply "Let's see... how about a pair of:  #{pair.join(', ')}"
+        end
       end
 
       def remove_user(user)
-        redis.srem('pair_members', user)
+        redis.srem(REDIS_KEY, user)
       end
 
       def add_user(user)
-        redis.sadd('pair_members', user)
+        redis.sadd(REDIS_KEY, user)
+      end
+
+      def create_pair
+        members = redis.smembers(REDIS_KEY).shuffle
+        pair = members.take(2)
+        return nil if pair.size == 1
+        pair
       end
 
       Lita.register_handler(self)
+
+      private
+      def parse_name(response)
+        response.args[1..-1].join(' ')
+      end
     end
   end
 end
--- spec/lita/handlers/pair_spec.rb
+++ spec/lita/handlers/pair_spec.rb
@@ -2,18 +2,46 @@ require 'spec_helper'
 require 'pry'
 
 describe Lita::Handlers::Pair, lita_handler: true do
+  let(:redis) { Redis.new }
+  let(:robot) { Lita::Robot.new(registry) }
+  subject { described_class.new(robot) }
   describe 'routing' do
     it { is_expected.to route("pair add Ryan") }
     it { is_expected.to route("pair add Ryan Latta") }
     it { is_expected.to route("pair  add  Ryan") }
     it { is_expected.to route("pair remove Ryan") }
     it { is_expected.to route("pair  remove  Ryan") }
+
+    
+    it { is_expected.to route("pair one") }
+    it { is_expected.to route("pair  one") }
+    
   end
 
   describe 'behaviors' do
-    let(:redis) { Redis.new }
-    let(:robot) { Lita::Robot.new(registry) }
-    subject { described_class.new(robot) }
+    describe 'creating a pair' do
+
+      before(:each) do
+        subject.add_user 'Ryan'
+        subject.add_user 'Stephen'
+      end
+
+      it 'should create a pair when there are users' do
+        send_message 'pair one'
+        message = replies.last
+        expect(message).to include 'pair of: '
+        expect(message).to include 'Ryan'
+        expect(message).to include 'Stephen'
+      end
+      
+      it 'should tell me if there aren\'t enough people to pair' do
+        subject.remove_user('Ryan')
+        send_message 'pair one'
+        message = replies.last
+        expect(message).to_not include 'pair of: '
+        expect(message).to include 'Sorry, I can\'t make a pair out of one person. Try adding more people with pair add'
+      end
+    end
 
     describe 'adding a pair' do
       it 'should add a user to the list of people when the add command is triggered' do
@@ -43,10 +71,6 @@ describe Lita::Handlers::Pair, lita_handler: true do
   end
 
   describe '#add_user' do
-    let(:redis) { Redis.new }
-    let(:robot) { Lita::Robot.new(registry) }
-    subject { described_class.new(robot) }
-
     it 'should add a named person to the redis cache' do
       subject.add_user('Ryan')
       members = subject.redis.smembers 'pair_members'
@@ -57,6 +81,29 @@ describe Lita::Handlers::Pair, lita_handler: true do
       subject.add_user('Ryan')
       members = subject.redis.smembers 'pair_members'
       expect(members.size).to equal(1)
+    end
+  end
+
+  describe '#create_pair' do
+    before(:each) do
+      subject.add_user('Ryan')
+      subject.add_user('Stephen')
+    end
+
+    it 'should choose among all the people in the list' do
+      subject.add_user 'Albert'
+      pair = subject.create_pair
+      members = subject.redis.smembers('pair_members')
+
+      expect(pair.size).to equal(2)
+      pair.each { |p| expect(members).to include(p) }
+    end
+
+    it 'should return nil if there are less than 2 people in the pool' do
+      subject.remove_user 'Albert'
+      subject.remove_user 'Stephen'
+      pair = subject.create_pair
+      expect(pair).to be_nil
     end
   end
 
